b0179ec606c51762ccfaa578eaea7b323da7d067
Nick Thiru <nickthiru@gmail.com>
2023-11-25 19:42:46 +0800

    Added MJML.

--- packages/bdd/src/service/email/email-template/price-change-alert-email-template.js
+++ packages/bdd/src/service/email/email-template/price-change-alert-email-template.js
@@ -1,25 +1,42 @@
-module.exports = emailTemplate = {
-  Source: "goldpricestracker@gmail.com",
-  Destination: {
-    ToAddresses: [
-      "goldpricestracker@gmail.com"
-    ],
-  },
-  ReplyToAddresses: [],
-  Message: {
-    Body: {
-      Html: {
-        Charset: "UTF-8",
-        Data: "<p>This is the body of my email.</p>",
-      },
-      Text: {
-        Charset: "UTF-8",
-        Data: "This is the body of my email!"
-      },
-    },
-    Subject: {
-      Charset: "UTF-8",
-      Data: "Hello",
-    },
-  },
-};
+exports.emailTemplate = `
+  <mjml>
+    <mj-head>
+      <mj-title>cloudcomponents - {{ title }}</mj-title>
+    </mj-head>
+    <mj-body>
+      <mj-section>
+        <mj-column>
+          <mj-text>
+            Hello {{ name }}!
+          </mj-text>
+        </mj-column>
+      </mj-section>
+    </mj-body>
+  </mjml>
+`;
+
+// module.exports = emailTemplate = {
+//   Source: "goldpricestracker@gmail.com",
+//   Destination: {
+//     ToAddresses: [
+//       "goldpricestracker@gmail.com"
+//     ],
+//   },
+//   ReplyToAddresses: [],
+//   Message: {
+//     Body: {
+//       Html: {
+//         Charset: "UTF-8",
+//         Data: "<p>This is the body of my email.</p>",
+//       },
+//       Text: {
+//         Charset: "UTF-8",
+//         Data: "This is the body of my email!"
+//       },
+//     },
+//     Subject: {
+//       Charset: "UTF-8",
+//       Data: "Hello",
+//     },
+//   },
+// };
